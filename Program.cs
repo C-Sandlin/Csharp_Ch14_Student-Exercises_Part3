b3be742ebd7e4b554aa8de0a27543016ebe0d1ed
Colin Sandlin <csandlin010@gmail.com>
2019-07-15 11:24:51 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace student_exercises_pt3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
